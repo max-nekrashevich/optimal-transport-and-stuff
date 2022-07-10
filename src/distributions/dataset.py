66a72139112c025338983e0ca486a0558372c6a2
Max Nekrashevich <max.v.nekrashevich@gmail.com>
2022-07-10 22:09:58 +0000

    Added tesor storing on CPU

--- src/distributions/dataset.py
+++ src/distributions/dataset.py
@@ -10,23 +10,27 @@ __all__ = ["TensorDatasetDistribution"]
 
 
 class TensorDatasetDistribution(CompositeDistribution):
-    def __init__(self, features, labels, device):
-        self.features = _to_tensor(features, device)
-        self.labels = _to_tensor(labels, device)
+    def __init__(self, features, labels, device, store_on_device=True):
+        self.store_on_device = store_on_device
+        self.features = _to_tensor(features, None)
+        self.labels = _to_tensor(labels, None)
         super().__init__(self.features.shape[1:], self.labels.unique(), device)
+        self.to(self.device)
 
     def to(self, device):
         super().to(device)
-        self.features.to(device)
-        self.labels.to(device)
+        if self.store_on_device:
+            self.features.to(device)
+            self.labels.to(device)
 
     def sample(self, sample_shape=torch.Size(), *, return_labels=False):
         sample_shape = torch.Size(sample_shape)
         indices = torch.randint(0, self.features.size(0), sample_shape)
 
         if return_labels:
-            return self.features[indices], self.labels[indices]
-        return self.features[indices]
+            return (self.features[indices].to(self.device),
+                    self.labels[indices].to(self.device))
+        return self.features[indices].to(self.device)
 
     def sample_components(self, sample_shape=torch.Size(), from_components=None):
         sample_shape = torch.Size(sample_shape)
@@ -38,7 +42,8 @@ class TensorDatasetDistribution(CompositeDistribution):
         indices = []
         for label in from_components:
             component = torch.where(self.labels == label)[0]
-            indices.append(component[torch.randint(0, component.size(0), sample_shape)])
+            indices.append(component[torch.randint(0, component.size(0),
+                                                   sample_shape)])
         indices = torch.stack(indices)
 
-        return self.features[indices]
+        return self.features[indices].to(self.device)
